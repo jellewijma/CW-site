fe389b54ed91c723522b15056ad403c509b57e78
Jelle Wijma <jelle.wijma@gmail.com>
2024-11-14 13:00:57 +0100

    update

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      keyframes: {
+        "fade-in-right": {
+          "0%": {
+            opacity: 0,
+            transform: "translate3d(100%, 0, 0)",
+          },
+          "100%": {
+            opacity: 1,
+            transform: "translate3d(0, 0, 0)",
+          },
+        },
+      },
+      animation: {
+        fadeinright: 'fade-in-right 1s ease-in-out 0.25s 1',
+      }
+    },
+  },
+  plugins: [],
+}
+
